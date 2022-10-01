474cfade6d4769a7fea52a8441d4a2063a82b3fc
Benjamin Lawton <106443433+lawtonb16@users.noreply.github.com>
2022-10-01 06:45:57 -0500

    Added Navigation

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import Main from "./screens/MainComponent";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <Main />
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
