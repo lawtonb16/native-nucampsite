4c38df13d48e575ed5cba30373856dade9275be9
Benjamin Lawton <106443433+lawtonb16@users.noreply.github.com>
2022-10-01 07:30:43 -0500

    Added Featured Items to Home Screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,54 @@
 import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import { CAMPSITES } from "../shared/campsites";
+import { PROMOTIONS } from "../shared/promotions";
+import { PARTNERS } from '../shared/partners';
 
+const FeaturedItem = ({ item }) => {
+    if (item) {
+        return (
+            <Card containerStyle={{ padding: 0 }}>
+                <Card.Image source={item.image}>
+                    <View style={{ justifyContent: 'center', flex: 1}}>
+                        <Text
+                            style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 20
+                            }}
+                        >
+                            {item.name}
+                        </Text>
+                    </View>
+                </Card.Image>
+                <Text style={{
+                    margin: 20
+                }}>
+                    {item.description}
+                </Text>
+            </Card>
+        )
+    }
+    return <View />
+}
+ 
 const HomeScreen = () => {
+    const [campsites , setCampsites] = useState(CAMPSITES);
+    const [promotions , setPromotions] = useState(PROMOTIONS);
+    const [partners , setPartners] = useState(PARTNERS);
+
+    const featCampsite = campsites.find(item => item.featured);
+    const featPromotions = promotions.find(item => item.featured);
+    const featPartners = partners.find(item => item.featured);
+
     return ( 
-        <View>
-            <Text>
-                Home Screen
-            </Text>
-        </View>
+        <ScrollView>
+            <FeaturedItem item={featCampsite} />
+            <FeaturedItem item={featPromotions} />
+            <FeaturedItem item={featPartners} />
+        </ScrollView>
      );
 }
  
