64123b1f266c27e05e40559bb076ce2bd45a7e79
Benjamin Lawton <106443433+lawtonb16@users.noreply.github.com>
2022-10-14 12:17:33 -0500

    Added Animation To Mutiple Screens

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { Rating } from "react-native-ratings";
 import { Input } from "react-native-elements";
 import { postComment } from "../features/comments/commentsSlice";
+import * as Animatable from "react-native-animatable";
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
@@ -53,7 +54,7 @@ const CampsiteInfoScreen = ({ route }) => {
     };
 
     return (
-        <>
+        <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
             <FlatList
                 data={comments.commentsArray.filter(
                     (comment) => comment.campsiteId === campsite.id,
@@ -129,7 +130,7 @@ const CampsiteInfoScreen = ({ route }) => {
                     </View>
                 </View>
             </Modal>
-        </>
+        </Animatable.View>
     );
 };
 
