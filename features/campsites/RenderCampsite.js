31123f475eaac519dd7fcee075c4762dc4b74a0b
Benjamin Lawton <106443433+lawtonb16@users.noreply.github.com>
2022-10-15 09:32:06 -0500

    Updated is RightSwipe

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -9,7 +9,7 @@ const RenderCampsite = (props) => {
 
     const view = useRef();
 
-    const isRightSwipe = ({ dx }) => dx < 200;
+    const isRightSwipe = ({ dx }) => dx > -200;
     const isLeftSwipe = ({ dx }) => dx < -200;
 
     const panResponder = PanResponder.create({
