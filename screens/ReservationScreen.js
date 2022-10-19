d909242750276a60711f271207703676425a4bb4
Benjamin Lawton <106443433+lawtonb16@users.noreply.github.com>
2022-10-18 19:24:19 -0500

    Added Notfications to reservation screen

--- screens/ReservationScreen.js
+++ screens/ReservationScreen.js
@@ -11,6 +11,7 @@ import {
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
 
 const ReservationScreen = () => {
     const [campers, setCampers] = useState(1);
@@ -44,7 +45,12 @@ const ReservationScreen = () => {
                 },
                 {
                     text: "OK",
-                    onPress: () => console.log("Ok Pressed"),
+                    onPress: () => {
+                        presentLocalNotification(
+                            date.toLocaleDateString("en-US"),
+                        );
+                        resetForm();
+                    },
                 },
             ],
             { cancelable: true },
@@ -57,6 +63,33 @@ const ReservationScreen = () => {
         setDate(new Date());
         setShowCalender(false);
     };
+
+    const presentLocalNotification = async (reservationDate) => {
+        const sendNotification = () => {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true,
+                    shouldPlaySound: true,
+                    shouldSetBadge: true,
+                }),
+            });
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: "Your Campsite Reservation Search",
+                    body: `Search for ${reservationDate} requested`,
+                },
+                trigger: null,
+            });
+        };
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    };
+
     return (
         <ScrollView>
             <Animatable.View duration={2000} animation="zoomIn" delay={1000}>
